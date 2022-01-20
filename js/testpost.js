1f82f35083a1301a1d5e55994853ff7486c21c9a
Emanuel Lucaci <emanuel.lucaci@solarwinds.com>
2022-01-20 14:13:07 +0100

    Add title and count to elements

--- js/testpost.js
+++ js/testpost.js
@@ -5,10 +5,14 @@ fetch('https://jsonplaceholder.typicode.com/posts', { method: 'GET', headers: { 
         }
         return data.json();
     }).then(data => {
+        
         var x = document.getElementsByClassName('motto');
+        var title = document.createElement('div');
+        title.innerHTML = 'Fetch Get method';
+        x.appendChild(title);
         for (index = 0; index < data.length; index++) {
             var e = document.createElement('div');
-            e.innerHTML = data[index].title;
+            e.innerHTML = '' + i + '   ' + data[index].title;
             x[0].appendChild(e);
         }
     }).catch(e => {
